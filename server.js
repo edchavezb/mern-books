2461e69f3af22ef683fdf72a730f396714cb1780
Magpie <edchavez821@gmail.com>
2019-07-26 13:21:06 -0500

    Small changes for Heroku deploy

--- server.js
+++ server.js
@@ -14,11 +14,6 @@ app.use(express.json());
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 // Define API routes here
 app.get("/library", function (req, res) {
   // TODO: Finish the route so it grabs all of the articles
@@ -60,11 +55,18 @@ app.post("/library/:id", function (req, res) {
     });
 });
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
 // Send every other request to the React app
 // Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
